ce9fa17034eb05b323125aabc875f497cf17771a
张梦思 <547151644@qq.com>
2021-04-11 14:53:47 +0800

    1

---
+++ 20/1.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = ['a', 'b', 'c', 'd'];
+// var_dump($arr);
+// 取值
+// echo $arr[1];
+// 赋值
+$arr[4] = 'e';
+// echo $arr[4];
+// unset($arr[2]);
+// 修改一个元素
+$arr[1] = 'php';
+var_dump($arr);
